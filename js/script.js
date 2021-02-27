0b2949653a9506fea343880e5df56b90a8649043
KyleLynnRas <kylelynn.ras@gmail.com>
2021-02-27 14:43:21 -0500

    added input and event listener

--- js/script.js
+++ js/script.js
@@ -1,11 +1,19 @@
 //char-stats variables 
 let url = "https://swapi.dev/api/people/?search="
+let $input = $(".char-input")
+let inputVal
 
+//event listener func on submit button
+$(".submit").on("click", getCharStats)
 
-//function for retrieving character data 
-const getCharStats = () => {
+//event listener for retrieving character data 
+function getCharStats(e) {
+    //prevent page refresh
+    e.preventDefault()
+    //char name from input
+    inputVal = $input.val()
     //pull from api url
-    $.ajax(url + "lando")
+    $.ajax(url + inputVal)
     //success: 
     .then(function(stats) {
         console.log(stats)
@@ -15,3 +23,4 @@ const getCharStats = () => {
         console.log(error)
     })
 }
+
