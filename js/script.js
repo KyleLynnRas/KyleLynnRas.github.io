223b1d286305294da166ddb55d4ba4467aa703d3
KyleLynnRas <kylelynn.ras@gmail.com>
2021-02-27 13:41:38 -0500

    added function to get character info from api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// alert("it works!")
+//char-stats variables 
+let url = "https://swapi.dev/api/people/?search="
+
+
+//function for retrieving character data 
+const getCharStats = () => {
+    //pull from api url
+    $.ajax(url + "lando")
+    //success: 
+    .then(function(stats) {
+        console.log(stats)
+    }, 
+    //error:
+    function(error) {
+        console.log(error)
+    })
+}
